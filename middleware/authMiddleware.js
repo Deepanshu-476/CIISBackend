fde65cc2c8156ad9d895d6d2fc62c10df8c5a348
Deepanshu <deep.nalhera476@gmail.com>
2026-07-10 17:15:58 +0530

    changes suparadmin

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -13,7 +13,7 @@ exports.verify = async (req, res) => {
     }
  
     if (!token) {
-      return res.status(401).json({
+      return res.status(401).json({    
         success: false,
         message: "No token provided"
       });
