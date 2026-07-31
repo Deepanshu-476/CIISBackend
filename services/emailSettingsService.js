e43ebd9038a7e26999042fae22862f6869bac633
Deepanshu <deep.nalhera476@gmail.com>
2026-07-31 18:23:47 +0530

    pull changes

--- services/emailSettingsService.js
+++ services/emailSettingsService.js
@@ -6,7 +6,7 @@ const SETTINGS_KEY = "global";
 let cachedSettings = null;
 let cachedAt = 0;
 let cachedIncludesSecret = false;
-const CACHE_TTL_MS = 5000;  
+const CACHE_TTL_MS = 5000;    
 const DEFAULT_PROFILE_ID = "default";
 
 const EMAIL_MODULES = [
